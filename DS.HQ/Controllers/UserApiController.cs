2e7c653abb18dd168a006c4b5cdea87d9d6dc68a
smallbenji <benjaminkfalch@gmail.com>
2026-05-14 00:16:55 +0200

    update HQ

---
+++ DS.HQ/Controllers/UserApiController.cs
@@ -0,0 +1,59 @@
+using DS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DS.HQ.Controllers
+{
+    public class UserApiController : Controller
+    {
+        private readonly DataDbContext dataDb;
+
+        public UserApiController(DataDbContext dataDb)
+        {
+            this.dataDb = dataDb;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var retval = dataDb.Groups.ToList();
+
+            return Ok(retval);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGroup([FromBody] Group data)
+        {
+            dataDb.Groups.Add(data);
+
+            await dataDb.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut("/{id}")]
+        public async Task<IActionResult> UpdateGroup([FromBody] Group data)
+        {
+            dataDb.Groups.Update(data);
+
+            await dataDb.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var group = await dataDb.Groups.FindAsync(id);
+
+            if (group == null)
+            {
+                return NotFound($"Group with ID {id} not found.");
+            }
+
+            dataDb.Groups.Remove(group);
+            await dataDb.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
