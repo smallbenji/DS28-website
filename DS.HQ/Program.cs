f7cfa068714de27d0e5283bfc5a5552cb4485347
smallbenji <benjaminkfalch@gmail.com>
2026-05-14 01:21:13 +0200

    application now migrates :D

--- DS.HQ/Program.cs
+++ DS.HQ/Program.cs
@@ -2,13 +2,22 @@ using DS;
 using DS.HQ;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddDSAuth(builder.Configuration);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options => {
+    var policy = new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser()
+                        .Build();
+
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
 
 builder.Services.AddSingleton<KeycloakHelper>();
 
@@ -32,5 +41,18 @@ app.MapGet("/logout", async context =>
 });
 
 app.MapFallbackToFile("dist/index.html");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try {
+        var context = services.GetRequiredService<DataDbContext>();
+        context.Database.Migrate();
+    } catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 app.Run();
