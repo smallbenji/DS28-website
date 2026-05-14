585deb4ca1608a75ec9c8c18d5b33b3cdbf197ff
smallbenji <benjaminkfalch@gmail.com>
2026-05-14 03:26:37 +0200

    cleanup

--- DS.HQ/Models/UserModels.cs
+++ DS.HQ/Models/UserModels.cs
@@ -1,21 +1,12 @@
+using NETCore.Keycloak.Client.Models.Groups;
+using NETCore.Keycloak.Client.Models.Users;
+
 namespace DS.HQ.Controllers
 {
     public class DSUser
     {
-        public NETCore.Keycloak.Client.Models.Users.KcUser User { get; set; }
+        public KcUser User { get; set; }
         public string GroupNumber { get; set; }
-        public List<NETCore.Keycloak.Client.Models.Groups.KcGroup> Roles { get; set; }
-    }
- 
-    public class UserIndexViewModel
-    {
-        public List<DSUser> Users { get; set; }
-    }
- 
-    public class UserDetailsViewModel
-    {
-        public DSUser DSUser { get; set; }
-        public List<NETCore.Keycloak.Client.Models.Groups.KcGroup> AvailableGroups { get; set; }
-        public List<string> SelectedGroups { get; set; }
+        public List<KcGroup> Roles { get; set; }
     }
 }
