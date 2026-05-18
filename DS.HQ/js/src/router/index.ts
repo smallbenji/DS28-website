a24d887f74b3c229cdf7c6ac041bc4ec12e7bc16
smallbenji <benjaminkfalch@gmail.com>
2026-05-18 21:04:42 +0200

    added loading

--- DS.HQ/js/src/router/index.ts
+++ DS.HQ/js/src/router/index.ts
@@ -1,40 +1,71 @@
 import { useGroupStore } from "@/Stores/GroupStore";
 import { useUserStore } from "@/Stores/UserStore";
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import { useLoading } from "buefy";
 
 const routes: RouteRecordRaw[] = [
     {
         path: "/user",
         component: () => import("@/Views/User.vue"),
         beforeEnter: async () => {
-            const userStore = useUserStore();
-            await userStore.GET_USERS();
+            const Loading = useLoading();
+            const loading = Loading.open({});
+
+            try {
+                const userStore = useUserStore();
+                await userStore.GET_USERS();
+                return true;
+            } catch {
+                return false;
+            } finally {
+                loading.close();
+            }
         }
     },
     {
         path: "/userv2",
         component: () => import("@/Views/UserV2.vue"),
         beforeEnter: async () => {
+            const Loading = useLoading();
+            const loading = Loading.open({});
             const userStore = useUserStore();
-            await userStore.GET_USERS();
-            await userStore.GET_GROUPS();
-
             const groupStore = useGroupStore();
-            await groupStore.GET_GROUPS();
+
+            try {
+                await userStore.GET_USERS();
+                await userStore.GET_GROUPS();
+                await groupStore.GET_GROUPS();
+
+                return true;
+            } catch {
+                return false;
+            } finally {
+                loading.close();
+            }
         }
     },
     {
         path: "/group",
         component: () => import("@/Views/Group.vue"),
         beforeEnter: async () => {
-            const groupStore = useGroupStore();
-            await groupStore.GET_GROUPS();
+            const Loading = useLoading();
+            const loading = Loading.open({});
+
+            try {
+                const groupStore = useGroupStore();
+                await groupStore.GET_GROUPS();
+
+                return true;
+            } catch {
+                return false;
+            } finally {
+                loading.close();
+            }
         }
     }
 ];
 
 const router = createRouter({
-
     history: createWebHistory(),
     routes
 });
