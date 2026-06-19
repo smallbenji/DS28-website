8021365016b4b44e867a400a76aaee83252b13de
smallbenji <benjaminkfalch@gmail.com>
2026-06-19 22:43:46 +0200

    cleanup

--- DS.HQ/Controllers/InvitationController.cs
+++ DS.HQ/Controllers/InvitationController.cs
@@ -11,7 +11,7 @@ namespace DS.HQ.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
-                var filePath = Path.Combine(env.ContentRootPath, "wwwroot", "dist", "index.html");
+            var filePath = Path.Combine(env.ContentRootPath, "wwwroot", "dist", "index.html");
 
             return PhysicalFile(filePath, "text/html");
         }
