4fcf8d8214ef47b6cf3beb2e4e397b3355331cf9
smallbenji <benjaminkfalch@gmail.com>
2026-06-25 16:41:18 +0200

    cleanup

--- DS/Models/UserInvitation.cs
+++ DS/Models/UserInvitation.cs
@@ -1,14 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace DS.Models
+namespace DS.Models;
+
+public class UserInvitation
 {
-    public class UserInvitation
-    {
-        [Key]
-        public int Id { get; set; }
-        public Guid InvitationId { get; set; }
-        public string Email { get; set; }
-        public List<string> Roles { get; set; }
-        public bool Used { get; set; } = false;
-    }
+    [Key]
+    public int Id { get; set; }
+    public Guid InvitationId { get; set; }
+    public string Email { get; set; }
+    public List<string> Roles { get; set; }
+    public bool Used { get; set; } = false;
 }
