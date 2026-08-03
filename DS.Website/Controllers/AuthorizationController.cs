dc508839d496b6a77743100b6ea06f815a30b1f5
Smallbenji <benjaminkfalch@gmail.com>
2026-08-03 03:43:02 +0200

    e

--- DS.Website/Controllers/AuthorizationController.cs
+++ DS.Website/Controllers/AuthorizationController.cs
@@ -24,7 +24,7 @@ public class AuthorizationController : Controller
     [IgnoreAntiforgeryToken]
     public async Task<IActionResult> Authorize()
     {
-        var request = HttpContext.GetOpenIddictServerRequest() ?? 
+        var request = HttpContext.GetOpenIddictServerRequest() ??
             throw new InvalidOperationException("OIDC anmodningen kunne ikke hentes.");
 
         if (!User.Identity.IsAuthenticated)
@@ -50,7 +50,7 @@ public class AuthorizationController : Controller
 
         // 3. VIGTIGT: OpenIddict skal vide præcis, hvilket ID der er 'subject' (sub)
         var userId = await _userManager.GetUserIdAsync(user);
-        
+
         // Vi tilføjer eksplicit sub claimet, hvis det ikke sidder rigtigt i forvejen
         var identity = (ClaimsIdentity)principal.Identity!;
         if (!principal.HasClaim(OpenIddictConstants.Claims.Subject))
@@ -80,7 +80,23 @@ public class AuthorizationController : Controller
 
         if (request.IsAuthorizationCodeGrantType())
         {
-            var principal = (await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)).Principal;
+            // 1. Hent det principal, som OpenIddict gemte i krypteret form i din 'code'
+            var result = await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            var principal = result.Principal;
+
+            if (principal == null)
+            {
+                return Forbid(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            }
+
+            // 2. OpenIddict kræver, at destinationerne OGSÅ gen-sættes under token-ombytningen
+            var identity = (ClaimsIdentity)principal.Identity!;
+            foreach (var claim in identity.Claims)
+            {
+                claim.SetDestinations(GetDestinations(claim, request));
+            }
+
+            // 3. Send det endelige token tilbage til WordPress
             return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
         }
 
