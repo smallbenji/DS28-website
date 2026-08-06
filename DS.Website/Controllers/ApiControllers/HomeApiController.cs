ad9b757e2bac8ffe4ee84051b1a2a3426e7991b8
smallbenji <benjaminkfalch@gmail.com>
2026-08-06 20:55:24 +0200

    Updating HomeApiController

--- DS.Website/Controllers/ApiControllers/HomeApiController.cs
+++ DS.Website/Controllers/ApiControllers/HomeApiController.cs
@@ -19,12 +19,12 @@ namespace DS.Website.Controllers
             new() { Title = "Wordpress", Url = "https://distriktssommerlejr.dk", Icon = ["fab", "wordpress"] },
 
             // Not in use
-            // new() { Title = "Audit log", URL = "#", Icon = "fa-solid fa-file-lines", RequiredRole = nameof(AppRoles.AuditLogView) },
-            // new() { Title = "Materialesystem", URL = "#", Icon = "fa-solid fa-cart-plus", RequiredRole = nameof(AppRoles.GroupDelete) },
-            // new() { Title = "Økonomi", URL = "#", Icon = "fa-solid fa-money-check-dollar", RequiredRole = nameof(AppRoles.GroupDelete) },
-            // new() { Title = "Tilmeldingssystem", URL = "#", Icon = "fa-solid fa-plus-circle" },
-            // new() { Title = "Grafana", URL = "#", Icon = "fa-solid fa-arrow-trend-up" },
-            // new() { Title = "Aktivitetsmodul", URL = "#", Icon = "fa-solid fa-newspaper" },
+            // new() { Title = "Audit log", Url = "#", Icon = ["fa-solid fa-file-lines"], RequiredRole = nameof(AppRoles.AuditLogView) },
+            // new() { Title = "Materialesystem", Url = "#", Icon = ["fa-solid fa-cart-plus"], RequiredRole = nameof(AppRoles.GroupsDelete) },
+            // new() { Title = "Økonomi", Url = "#", Icon = ["fa-solid fa-money-check-dollar"], RequiredRole = nameof(AppRoles.GroupsDelete) },
+            // new() { Title = "Tilmeldingssystem", Url = "#", Icon = ["fa-solid fa-plus-circle"] },
+            // new() { Title = "Grafana", Url = "#", Icon = ["fa-solid fa-arrow-trend-up"] },
+            // new() { Title = "Aktivitetsmodul", Url = "#", Icon = ["fa-solid fa-newspaper"] },
         ];
 
         [HttpGet]
