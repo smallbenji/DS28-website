6555e5f60e3404f6206de2a22ba8a17713cf8eac
smallbenji <benjaminkfalch@gmail.com>
2026-08-07 03:10:54 +0200

    ssl for viter

--- DS.Website/js/vite.config.ts
+++ DS.Website/js/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
 import VitePluginVueDevTools from 'vite-plugin-vue-devtools'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    plugins: [vue(), VitePluginVueDevTools()],
+    plugins: [vue(), basicSsl(), VitePluginVueDevTools()],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url))
