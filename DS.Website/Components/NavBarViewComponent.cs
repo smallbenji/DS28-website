966a13ea57217948ee75e7d5986a1c0917d286b0
smallbenji <benjaminkfalch@gmail.com>
2026-08-11 01:23:52 +0200

    cleanup

--- DS.Website/Components/NavBarViewComponent.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace DS.Website.Components
-{
-    public class NavBarViewComponent : ViewComponent
-    {
-        public async Task<IViewComponentResult> InvokeAsync() => View();
-    }
-}
--- DS.Website/Controllers/AnonymousController.cs
+++ DS.Website/Controllers/AnonymousController.cs
@@ -4,13 +4,19 @@ using Microsoft.AspNetCore.Mvc;
 namespace DS.Website.Controllers
 {
     [AllowAnonymous]
-    public class AuthPageController(IWebHostEnvironment env) : Controller
+    public class AnonymousController(IWebHostEnvironment env) : Controller
     {
         [HttpGet("/login")]
         public IActionResult Login() => SpaIndex();
 
         [HttpGet("/register")]
         public IActionResult Register() => SpaIndex();
+
+        [HttpGet("/invitation/{id}")]
+        public IActionResult Invitation() => SpaIndex();
+
+        [HttpGet("/reset-password/{id}")]
+        public IActionResult ResetPassword() => SpaIndex();
 
         private IActionResult SpaIndex()
         {
