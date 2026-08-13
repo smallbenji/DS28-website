279aab55bf7f48faa02fd994a85e4865bb3897c1
smallbenji <benjaminkfalch@gmail.com>
2026-08-13 16:50:17 +0200

    merge fixes

--- DS.Website/js/src/Services/MeService.ts
+++ DS.Website/js/src/Services/MeService.ts
@@ -10,9 +10,9 @@ export default class MeService {
                 method: "GET"
             });
 
-            return response.data ? response.data : {name: "", firstName: "", lastName: "", mustEnableTwoFactor: false, roles: [], appRoles: [], isAuthenticated: false, passkeys: []};
+            return response.data ? response.data : {id: "", name: "", firstName: "", lastName: "", mustEnableTwoFactor: false, roles: [], appRoles: [], isAuthenticated: false, passkeys: []};
         } catch {
-            return { name: "", firstName: "", lastName: "", mustEnableTwoFactor: false, roles: [], appRoles: [], isAuthenticated: false, passkeys: [] };
+            return { id: "", name: "", firstName: "", lastName: "", mustEnableTwoFactor: false, roles: [], appRoles: [], isAuthenticated: false, passkeys: [] };
         }
     }
 
