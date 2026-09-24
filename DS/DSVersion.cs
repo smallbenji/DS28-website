5a1f1695eac944a71e8f233fc9dbaeef71603be1
smallbenji <benjaminkfalch@gmail.com>
2026-09-24 23:11:52 +0200

    Update version

--- DS/DSVersion.cs
+++ DS/DSVersion.cs
@@ -2,7 +2,7 @@ namespace DS
 {
     public static class DSVersion
     {
-        public const string Version = "0.1.3";
+        public const string Version = "0.1.4";
         public const string Name = "DS28 Website";
     }
 }
